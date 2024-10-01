79c842a6e0ca8ae9b72c9159b93f5260c2efd763
Luis Castillo <luisc58@Luiss-MacBook-Pro.local>
2024-09-30 23:16:01 -0400

    Modify varables

--- main.tf
+++ main.tf
@@ -15,7 +15,9 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_s3_bucket" "example_bucket" {
